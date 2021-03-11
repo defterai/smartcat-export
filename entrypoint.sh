296c5db05f546a31a988565e1f4132dd6c809fe7
Defter <defter@bigmir.net>
2021-03-11 23:23:50 +0200

    Fix path to python script

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,6 @@ set -eu
 
 printf "\nExport document %s:%s to %s...\n" "$3" "$4" "$5"
 
-python3 export-smartcat.py $1 $2 $3 $4 $5 || { printf "\nUnable to export %s document.\n" "$5"; exit 1; }
+python3 /export-smartcat.py $1 $2 $3 $4 $5 || { printf "\nUnable to export %s document.\n" "$5"; exit 1; }
 
 printf "\nSuccessfully exported %s file.\n" "$5"
